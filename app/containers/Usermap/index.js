d68da3a4f0644645213df6cfc02e454071eef78b
Deepanshu Bansal <bansaldeepanshu08@gmail.com>
2019-10-15 11:58:47 +0530

    Google map refresh err

--- app/containers/Usermap/index.js
+++ app/containers/Usermap/index.js
@@ -22,17 +22,87 @@ export function Usermap({getUserLocation, location, putLocation}) {
   useInjectReducer({ key: 'usermap', reducer });
   useInjectSaga({ key: 'usermap', saga });
 
-  const [center, setCenter] = useState({
-    lat: 59.95,
-    lng: 30.33
-  });
-  var id = 0;
+  // const [center, setCenter] = useState({
+  //   lat: 59.95,
+  //   lng: 30.33
+  // });
+  // var id = 0;
+
+  // Hook
+  let cachedScripts = [];
+  function useScript(src) {
+    // Keeping track of script loaded and error state
+    const [state, setState] = useState({
+      loaded: false,
+      error: false
+    });
+
+    useEffect(
+      () => {
+        // If cachedScripts array already includes src that means another instance ...
+        // ... of this hook already loaded this script, so no need to load again.
+        if (cachedScripts.includes(src)) {
+          setState({
+            loaded: true,
+            error: false
+          });
+        } else {
+          cachedScripts.push(src);
+
+          // Create script
+          let script = document.createElement('script');
+          script.src = src;
+          script.async = true;
+
+          // Script event listener callbacks for load and error
+          const onScriptLoad = () => {
+            setState({
+              loaded: true,
+              error: false
+            });
+          };
+
+          const onScriptError = () => {
+            // Remove from cachedScripts we can try loading again
+            const index = cachedScripts.indexOf(src);
+            if (index >= 0) cachedScripts.splice(index, 1);
+            script.remove();
+
+            setState({
+              loaded: true,
+              error: true
+            });
+          };
+
+          script.addEventListener('load', onScriptLoad);
+          script.addEventListener('error', onScriptError);
+
+          // Add script to document body
+          document.body.appendChild(script);
+
+          // Remove event listeners on cleanup
+          return () => {
+            script.removeEventListener('load', onScriptLoad);
+            script.removeEventListener('error', onScriptError);
+          };
+        }
+      },
+      [src] // Only re-run effect if script src changes
+    );
+
+    return [state.loaded, state.error];
+  }
   
  
+  const [loaded, error] = useScript(
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyDiWFGyuSGusJbWlooWWyEACxTuOsomDJs&callback=initMap'
+  );
+
   useEffect(() => {
     // getUserLocation();
     fetchLocation();
-  }, [])
+    loaded ? myMap() : ''
+  }, [loaded])
 
   const fetchLocation = () => {
     navigator.geolocation.watchPosition(
@@ -43,20 +113,33 @@ export function Usermap({getUserLocation, location, putLocation}) {
     );
   }
 
-  const [zoom, setZoom] = useState(11);
+  // const [zoom, setZoom] = useState(11);
 
-  const imgStyle = {
-    height: '30px'
+  function myMap() {
+    var mapCanvas = document.getElementById("mapsCanvas");
+    var mapOptions = {
+      center: {lat: 19.0587757, lng: 76.085601},
+      zoom: 12
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({position: location, map: map});
+    map.setCenter(location);
   }
 
-  const AnyReactComponent = ({ text }) => 
-    <div>
-    <img style={imgStyle} alt={text} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" />
-      {text}
-    </div>;
+  // const imgStyle = {
+  //   height: '30px'
+  // }
+
+  // const AnyReactComponent = ({ text }) => 
+  //   <div>
+  //   <img style={imgStyle} alt={text} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" />
+  //     {text}
+  //   </div>;
 
   return (
-    <GoogleMap userLocation={location}/>
+    <div>
+      <div id="mapsCanvas"  style={{ height: 'calc(100vh - 80px)', width: '100%' }}></div>
+    </div>
   );
 }
 
